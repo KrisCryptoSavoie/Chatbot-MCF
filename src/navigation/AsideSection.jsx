6688c5ea721a300b1961ebaff0f7300c0ca8c8c0
Kris <110132229+KrisCryptoSavoie@users.noreply.github.com>
2023-01-25 20:51:07 +0100

    Add files via upload

---
+++ src/navigation/AsideSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function AsideSection({showConversationDebug, setShowConversationDebug}) {
+  return <section className="full md:hidden lg:flex lg:quarter lg:screen-v-scroll flex row wrap">
+    <div className="full md:py">
+      <br/>
+    </div>
+  </section>
+}
